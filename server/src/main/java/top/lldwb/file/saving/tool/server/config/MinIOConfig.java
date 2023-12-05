0cbcd39d32fb10cf40b8af9a1c9254e8ac1072c0
安然的尾巴 <3247187440@qq.com>
2023-12-05 15:04:21 +0800

    更新 MinIOConfig.java

--- server/src/main/java/top/lldwb/file/saving/tool/server/config/MinIOConfig.java
+++ server/src/main/java/top/lldwb/file/saving/tool/server/config/MinIOConfig.java
@@ -1,5 +1,6 @@
 package top.lldwb.file.saving.tool.server.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import top.lldwb.file.saving.tool.server.factory.MinioFactoryBean;
