e4b1f955f1eeb69ded8017bf05707479d85629f8
安然的尾巴 <3247187440@qq.com>
2024-01-04 20:42:53 +0800

    更新 LoginInterceptor.java

--- server/src/main/java/top/lldwb/file/saving/tool/server/controller/interceptor/LoginInterceptor.java
+++ server/src/main/java/top/lldwb/file/saving/tool/server/controller/interceptor/LoginInterceptor.java
@@ -1,10 +1,5 @@
 package top.lldwb.file.saving.tool.server.controller.interceptor;
 
-import cn.hutool.core.date.DateUtil;
-import cn.hutool.jwt.JWT;
-import cn.hutool.jwt.JWTHeader;
-import cn.hutool.jwt.JWTUtil;
-import cn.hutool.jwt.JWTValidator;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -20,15 +15,15 @@ import org.springframework.web.servlet.HandlerInterceptor;
 public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        String userId = (String) request.getAttribute("userId");
-        if (userId != null && !"".equals(userId)) {
-            // 检测会话是否过期
-            JWTValidator.of(userId).validateDate(DateUtil.date());
-            final JWT jwt = JWTUtil.parseToken(userId);
-
-            jwt.getHeader(JWTHeader.TYPE);
-            request.setAttribute("userId",jwt.getPayload("userId"));
-        }
+        String userId = request.getParameter("userId");
+//        if (userId != null && !"".equals(userId)) {
+//            // 检测会话是否过期
+//            JWTValidator.of(userId).validateDate(DateUtil.date());
+//            final JWT jwt = JWTUtil.parseToken(userId);
+//
+//            jwt.getHeader(JWTHeader.TYPE);
+//            request.setAttribute("userId",jwt.getPayload("userId"));
+//        }
         return true;
     }
 }
