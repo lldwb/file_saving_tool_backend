8e0d86219521996aa1c760ab3b97cba294c74238
安然的尾巴 <3247187440@qq.com>
2024-01-10 16:01:18 +0800

    jwt测试

--- server/src/test/java/top/lldwb/file/saving/tool/server/ServerApplicationTests.java
+++ server/src/test/java/top/lldwb/file/saving/tool/server/ServerApplicationTests.java
@@ -1,5 +1,10 @@
 package top.lldwb.file.saving.tool.server;
 
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.jwt.JWT;
+import cn.hutool.jwt.JWTHeader;
+import cn.hutool.jwt.JWTUtil;
+import cn.hutool.jwt.JWTValidator;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +15,21 @@ class ServerApplicationTests {
 
     @Test
     void contextLoads() {
-      log.info("test");
-    }
+        String jwtStr = JWT.create()
+                // 设置签发时间
+                .setIssuedAt(DateUtil.date())
+                // 设置过期时间
+                .setExpiresAt(DateUtil.date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 15))
+                .setKey("加盐".getBytes())
+                .setPayload("userId",123)
+                // 签名生成JWT字符串
+                .sign();
 
+        // 检测会话是否过期
+        JWTValidator.of(jwtStr).validateDate(DateUtil.date());
+        final JWT jwt = JWTUtil.parseToken(jwtStr);
+        jwt.getHeader(JWTHeader.TYPE);
+
+        log.info("jwt：{}",jwt.getPayload("userId"));
+    }
 }
